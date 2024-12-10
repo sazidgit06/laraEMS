f298f5191c6c8ca44b19efb2135a8723f4a368de
Sazid <sazidtec06@gmail.com>
2024-12-10 20:19:54 +0600

    second commit class name

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use app\Http\Controllers\employeeController;
+use app\Http\Controllers\EmployeeController;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::get('/',[employeeController::class,'index']);
+Route::get('/',[EmployeeController::class,'index']);
